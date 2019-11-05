c292ce69e1d7a2798bf48058e16f50e16b3cb67d
Grzegorz <grzegorz.sikorski.96@gmail.com>
2019-11-05 20:53:19 +0100

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-        $this->call(DeviceTableSeeder::class);
-        $this->call(MeasurementsTableSeeder::class);
+//        $this->call(DeviceTableSeeder::class);
+//        $this->call(MeasurementsTableSeeder::class);
     }
 }
